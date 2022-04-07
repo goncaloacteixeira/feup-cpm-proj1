3234ee818dc93c596c1a519c753caf252ca8f6e5
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-04-07 15:52:56 +0100

    added transactions fragment

--- AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/ui/transactions/TransactionsFragment.kt
+++ AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/ui/transactions/TransactionsFragment.kt
@@ -1,4 +1,4 @@
-package org.feup.cpm.group9.acmeshop.ui.gallery
+package org.feup.cpm.group9.acmeshop.ui.transactions
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import org.feup.cpm.group9.acmeshop.databinding.FragmentGalleryBinding
+import org.feup.cpm.group9.acmeshop.databinding.FragmentTransactionsBinding
 
-class GalleryFragment : Fragment() {
+class TransactionsFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentTransactionsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val transactionsViewModel =
+            ViewModelProvider(this).get(TransactionsViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentTransactionsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textTransactions
+        transactionsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/ui/transactions/TransactionsViewModel.kt
+++ AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/ui/transactions/TransactionsViewModel.kt
@@ -1,13 +1,13 @@
-package org.feup.cpm.group9.acmeshop.ui.gallery
+package org.feup.cpm.group9.acmeshop.ui.transactions
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class TransactionsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "This is transactions Fragment"
     }
     val text: LiveData<String> = _text
 }

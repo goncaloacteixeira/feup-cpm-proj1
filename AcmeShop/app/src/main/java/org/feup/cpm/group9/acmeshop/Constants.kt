a4ff229a6d23939215570bb7c6acd203144d9f47
Gonçalo Teixeira <goncalo.ac.teixeira@gmail.com>
2022-05-18 12:03:53 +0100

    fixed a few aspects on basket item dialog

--- AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/Constants.kt
+++ AcmeShop/app/src/main/java/org/feup/cpm/group9/acmeshop/Constants.kt
@@ -1,3 +1,4 @@
 package org.feup.cpm.group9.acmeshop
 
-const val API_URL = "http://192.168.1.127:3000"
+// 10.0.2.2 for localhost (if emulated on the same pc as server)
+const val API_URL = "http://10.0.2.2:3000"
